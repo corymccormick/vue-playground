18d3005103aadd9c6864a9c8d8c53aedb7a824fb
corymccormick <corym.otf@gmail.com>
2021-04-26 08:01:07 -0600

    com

--- src/components/ConditionalRenderingExercise.vue
+++ src/components/ConditionalRenderingExercise.vue
@@ -10,7 +10,7 @@
             screen.
           </p>
           <!-- add code to element below -->
-          <div class="player-card text-center">
+          <div class="player-card text-center" v-if="true">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -38,7 +38,7 @@
             toggle the value to true to get it to show up again.
           </p>
           <!-- add code to element below -->
-          <div class="player-card text-center">
+          <div class="player-card text-center" v-if="state.showPlayer">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -130,6 +130,7 @@ export default {
     const state = reactive({
       //add a property to toggle the player-card here.
       //add a property to set the number value here.
+      showPlayer: true,
       player: {
         photo: "https://robohash.org/D$",
         name: "D$",
